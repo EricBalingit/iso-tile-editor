d33443fd3714ce4c0c0859c1565fa3d71aaa1caf
ericbalingit <embalingit.github@gmail.com>
2017-01-20 19:05:01 +0000

    local & global rotation separate with update signaling

--- shapes/Shape3D.js
+++ shapes/Shape3D.js
@@ -19,15 +19,17 @@ define ( function ( require, exports, module ) {
      *      3D spline representation, which is nothing more than a flat
      *      array with and extra z = 0 for each point in the 2D path
      */
-    var Shape3D = function ( shape2D ) {
+    var Shape3D = function ( shape2D, position, rotation ) {
         
-        this.position = new Vec3D ();
-        this.rotation = new Rotation ( 1, 0, 0, 0 );
+        this.shape2D = shape2D;
+        this.position = position;
+        this.rotation = rotation;
         this.localEuler = new Vec3D ( 0, 0, 0 );
         this.globalEuler = new Vec3D ( 0, 0, 0 );
         this.shape = [];
         this.clip = [];
-        this.shape2D = shape2D;
+        
+        this.localRotationChanged = this.globalRotationChanged = false;
         
         this.update ();
     };
@@ -36,92 +38,6 @@ define ( function ( require, exports, module ) {
         
         var p = new Vec3D (), v = new Vec3D (), q = new Rotation (), r = new Rotation ();
         
-        proto.update = function update ( camera ) {
-            
-            var scale = this.shape2D.scale, rotation = this.rotation,
-                segments = this.segments, clip = this.clip,
-                shape = this.shape, position = this.position,
-                l = segments.length, i, j, t, seg,
-                sx = scale.x, sy = scale.y, sz = scale.z,
-                ax, ay, bx, by, cx, cy, dx, dy, px, py, lx, ly;
-            
-            shape.length = 0;
-            clip.length = 0;
-            
-            p.set ( position );
-            camera.rotate ( p, v );
-            lx = v.x; ly = v.y;
-            
-            for ( i = 0; i < l; i = i + 1 ) {
-                
-                seg = segments [ i ];
-                
-                if ( seg.length === 2 ) {
-                    ax = px = seg [ 0 ]; ay = py = seg [ 1 ];
-                    p.set ( px, py, 0 );
-                    rotation.rotate ( p, v );
-                    shape.push ( ax = sx * v.x + lx, ay = sy * v.y + ly );
-                    clip.push ( ax, ay );
-                } else if ( seg.length === 4 ) {
-                    ax = px;        ay = py;
-                    bx = seg [ 0 ]; by = seg [ 1 ];
-                    cx = seg [ 2 ]; cy = seg [ 3 ];
-                    px = cx;        py = cy;
-                    
-                    p.set ( ax, ay, 0 );
-                    rotation.rotate ( p, v );
-                    shape.push ( ax = sx * v.x + ly, ay = sy * v.y + ly );
-                    
-                    p.set ( bx, by, 0 );
-                    rotation.rotate ( p, v );
-                    shape.push ( bx = sx * v.x + lx, by = sy * v.y + ly );
-                    
-                    p.set ( cx, cy, 0 );
-                    rotation.rotate ( p, v );
-                    shape.push ( cx = sx * v.x + lx, cy = sy * v.y + ly );
-                    
-                    for ( j = 0; j <= 16; j = j + 1 ) {
-                        t = j / 16;
-                        clip.push (
-                            quadraticPoint ( ax, bx, cx, dx, t ),
-                            quadraticPoint ( ay, by, cy, dy, t )
-                        );
-                    }
-                    
-                } else {
-                    ax = px;        ay = py;
-                    bx = seg [ 0 ]; by = seg [ 1 ];
-                    cx = seg [ 2 ]; cy = seg [ 3 ];
-                    dx = seg [ 4 ]; dy = seg [ 5 ];
-                    px = dx;        py = dy;
-                    
-                    p.set ( ax, ay, 0 );
-                    rotation.rotate ( p, v );
-                    shape.push ( ax = sx * v.x + ly, ay = sy * v.y + ly );
-                    
-                    p.set ( bx, by, 0 );
-                    rotation.rotate ( p, v );
-                    shape.push ( bx = sx * v.x + lx, by = sy * v.y + ly );
-                    
-                    p.set ( cx, cy, 0 );
-                    rotation.rotate ( p, v );
-                    shape.push ( cx = sx * v.x + lx, cy = sy * v.y + ly );
-                    
-                    p.set ( dx, dy, 0 );
-                    rotation.rotate ( p, v );
-                    shape.push ( dx = sx * v.x + lx, dy = sy * v.y + ly );
-                    
-                    for ( j = 0; j <= 16; j = j + 1 ) {
-                        t = j / 16;
-                        clip.push (
-                            bezierPoint ( ax, bx, cx, dx, t ),
-                            bezierPoint ( ay, by, cy, dy, t )
-                        );
-                    }
-                }
-            }
-        };
-    
         proto.render = function render ( ctx ) {
             var segments = this.segments, shape = this.shape,
                 seg, i, l = segments.length, t = 2, s,
@@ -172,8 +88,128 @@ define ( function ( require, exports, module ) {
             ctx.restore ();
         };
         
+        proto.rotateLocalEuler = function rotateLocalEuler ( x, y, z ) {
+            this.localEuler.add.apply ( null, arguments );
+            this.localRotationChanged = true;
+        };
+        
+        proto.rotateGlobalEuler = function rotateGlobalEuler ( x, y, z ) {
+            this.globalEuler.add.apply ( null, arguments );
+            this.globalRotationChanged = true;
+        };
+        
         proto.toString = function toString () {
             serialize ( this );
+        };
+        
+        proto.update = function update ( camera ) {
+            
+            var scale = this.shape2D.scale, rotation = this.rotation,
+                local = this.localEuler, global = this.globalEuler,
+                segments = this.segments, clip = this.clip,
+                shape = this.shape, position = this.position,
+                l = segments.length, i, j, t, seg,
+                sx = scale.x, sy = scale.y, // scale.z is never used since shapes are planar
+                ax, ay, bx, by, cx, cy, dx, dy, px, py, lx, ly;
+            
+            shape.length = 0;
+            clip.length = 0;
+            
+            p.set ( position );
+            camera.rotate ( p, v );
+            lx = v.x; ly = v.y;
+            
+            var newRotation = false;
+            if ( this.localRotationChanged ) {
+                q.putEuler( local.x, local.y, local.z, false );
+                newRotation = true;
+                this.localRotationChanged = false;
+            } else {
+                q.put ( this.localRotation );
+            }
+            if ( this.globalRotationChanged ) {
+                r.putEuler ( global.x, global.y, global.z, false );
+                newRotation = true;
+                this.globalRotationChanged = false;
+            } else {
+                r.put ( this.globalRotation );
+            }
+            if ( newRotation ) {
+                // q * r * camera -> r
+                camera.setMult ( r.putMult ( q, r ), r );
+            } else {
+                // rotation * camera -> r
+                camera.setMult ( rotation, r );
+            }
+            
+            for ( i = 0; i < l; i = i + 1 ) {
+                
+                seg = segments [ i ];
+                
+                if ( seg.length === 2 ) {
+                    ax = px = seg [ 0 ]; ay = py = seg [ 1 ];
+                    p.set ( px, py, 0 );
+                    r.rotate ( p, v );
+                    shape.push ( ax = sx * v.x + lx, ay = sy * v.y + ly );
+                    clip.push ( ax, ay );
+                } else if ( seg.length === 4 ) {
+                    ax = px;        ay = py;
+                    bx = seg [ 0 ]; by = seg [ 1 ];
+                    cx = seg [ 2 ]; cy = seg [ 3 ];
+                    px = cx;        py = cy;
+                    
+                    p.set ( ax, ay, 0 );
+                    r.rotate ( p, v );
+                    shape.push ( ax = sx * v.x + ly, ay = sy * v.y + ly );
+                    
+                    p.set ( bx, by, 0 );
+                    r.rotate ( p, v );
+                    shape.push ( bx = sx * v.x + lx, by = sy * v.y + ly );
+                    
+                    p.set ( cx, cy, 0 );
+                    r.rotate ( p, v );
+                    shape.push ( cx = sx * v.x + lx, cy = sy * v.y + ly );
+                    
+                    for ( j = 0; j <= 16; j = j + 1 ) {
+                        t = j / 16;
+                        clip.push (
+                            quadraticPoint ( ax, bx, cx, dx, t ),
+                            quadraticPoint ( ay, by, cy, dy, t )
+                        );
+                    }
+                    
+                } else {
+                    ax = px;        ay = py;
+                    bx = seg [ 0 ]; by = seg [ 1 ];
+                    cx = seg [ 2 ]; cy = seg [ 3 ];
+                    dx = seg [ 4 ]; dy = seg [ 5 ];
+                    px = dx;        py = dy;
+                    
+                    p.set ( ax, ay, 0 );
+                    r.rotate ( p, v );
+                    shape.push ( ax = sx * v.x + ly, ay = sy * v.y + ly );
+                    
+                    p.set ( bx, by, 0 );
+                    r.rotate ( p, v );
+                    shape.push ( bx = sx * v.x + lx, by = sy * v.y + ly );
+                    
+                    p.set ( cx, cy, 0 );
+                    r.rotate ( p, v );
+                    shape.push ( cx = sx * v.x + lx, cy = sy * v.y + ly );
+                    
+                    p.set ( dx, dy, 0 );
+                    r.rotate ( p, v );
+                    shape.push ( dx = sx * v.x + lx, dy = sy * v.y + ly );
+                    
+                    for ( j = 0; j <= 16; j = j + 1 ) {
+                        t = j / 16;
+                        clip.push (
+                            bezierPoint ( ax, bx, cx, dx, t ),
+                            bezierPoint ( ay, by, cy, dy, t )
+                        );
+                    }
+                }
+            }
         };
         
     } ) ( Shape3D.prototype );
